0f7c205215bdb5097c4b190b2eb7f7f8cb7466f8
bealski93 <elizabethbeales93@gmail.com>
2019-11-13 20:21:19 +0000

    Added comment and title.

--- 12-11-2019-Cran-Programming-Languages-Tidy-Tuesday.R
+++ 12-11-2019-Cran-Programming-Languages-Tidy-Tuesday.R
@@ -1,22 +1,33 @@
+#load libraries
+
 library(tidyverse)
+
+#load data
+
 cran_code <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-11-12/loc_cran_packages.csv")
+
+#take a quick look at the data
 head(cran_code)
 summary(cran_code)
 
+#group data by language, summarise with total number of comments, then show the top 20 languages
 most_comment <- cran_code %>% 
   group_by(language) %>%
   summarise(total_comment = sum(comment)) %>%
   filter(dense_rank(desc(total_comment)) <=20 )
 
+#sort data
 most_comment_sorted <- arrange(most_comment, desc(total_comment))
 
+#create plot
 plot <- ggplot(most_comment_sorted, aes(x = language, y = total_comment, fill = factor(language))) +
   geom_col() +
   ylab("Total Number of Comments") +
   xlab("Programming Language") +
   coord_flip() +
   theme_light() +
-  theme(legend.position = "none") 
+  theme(legend.position = "none") +
+  ggtitle("The top 20 most commented programming languages within R packages")
   
 plot
   
